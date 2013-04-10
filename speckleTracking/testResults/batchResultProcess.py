d0eae18b16257a0dc067c31d0935a1c0fa0c23f9
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-10 08:58:40 +0200

    / ‘speckleTracking/testResults/batchResultProcess.py’

--- speckleTracking/testResults/batchResultProcess.py
+++ speckleTracking/testResults/batchResultProcess.py
@@ -2,5 +2,21 @@
 
 f = open("batchResult.txt")
 
+BEGIN = 1
+NAME = 2
+STDDEV = 3
+DIFF = 4
+
+name = ""
+meanStrain = 0.0
+perSegmentStrain = 0.0
+strainSum = 0.0
+diff = 0.0
+totalSum = 0.0
+state = BEGIN
+
 for line in f:
-	print(line)
+	if state == BEGIN:
+		index = line.find('"')
+		name = line[index:]
+		print(name)
