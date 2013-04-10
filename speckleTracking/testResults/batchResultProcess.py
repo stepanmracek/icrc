7efc68763bc80ad00fb3c245385a886b100b6e2d
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-10 09:42:42 +0200

    / ‘speckleTracking/testResults/batchResultProcess.py’

--- speckleTracking/testResults/batchResultProcess.py
+++ speckleTracking/testResults/batchResultProcess.py
@@ -13,6 +13,13 @@ perSegmentStrain = 0.0
 strainSum = 0.0
 diff = 0.0
 totalSum = 0.0
+
+minMeanStrain = 1e300
+minPerSegmentStrain = 1e300
+minStrainSum = 1e300
+minDiff = 1e300
+minTotalSum = 1e300
+
 state = BEGIN
 
 for line in f:
@@ -36,4 +43,25 @@ for line in f:
 		diff = float(items[1])
 		state = BEGIN
 
-		print(name)
+		if (meanStrain < minMeanStrain):
+			minMeanStrainName = name
+			minMeanStrain = meanStrain
+
+		if (perSegmentStrain < minPerSegmentStrain):
+			minPerSegmentStrain = perSegmentStrain
+			minPerSegmentStrainName = name
+
+		if (strainSum < minStrainSum):
+			minStrainSum = strainSum
+			minStrainSumName = name
+
+		if (diff < minDiff):
+			minDiff = diff
+			minDiffName = name
+
+		if (totalSum < minTotalSum):
+			minTotalSum = totalSum
+			minTotalSumName = name
+
+print("minMeanStrain: %f,\n", minMeanStrain)
+print(minMeanStrainName)
