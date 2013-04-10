43f73b4fc66f7f49351fd10ad6807b94a1b51831
Štěpán Mráček <stepan.mracek@gmai.com>
2013-04-10 09:28:24 +0200

    / ‘speckleTracking/testResults/batchResultProcess.py’

--- speckleTracking/testResults/batchResultProcess.py
+++ speckleTracking/testResults/batchResultProcess.py
@@ -17,25 +17,21 @@ state = BEGIN
 
 for line in f:
 	if state == BEGIN:
-		print("state BEGIN")
 		index = line.find('"')
 		name = line[index+1:]
 		state = NAME
 	elif state == NAME:
-		print("state NAME")
 		if line.strip() == '"':
 			state = STDDEV
 		else:
 			name = name + line
 	elif state == STDDEV:
-		print("state STDDEV")
 		items = line.split()
 		meanStrain = float(items[1])
 		perSegmentStrain = float(items[2])
 		strainSum = float(items[3])
 		state = DIFF
 	elif state == DIFF:
-		print("state DIFF")
 		items = line.split()
 		diff = float(items[1])
 		state = BEGIN
