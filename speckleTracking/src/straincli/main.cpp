0e300337d823cf879a5d4985a8728611149e4bcd
Stepan Mracek <stepan.mracek@gmail.com>
2015-08-11 22:27:50 +0200

    update main.cpp for straincli

--- speckleTracking/src/straincli/main.cpp
+++ speckleTracking/src/straincli/main.cpp
@@ -2,6 +2,7 @@
 
 #include "batchstraintracker.h"
 #include "createmodel.h"
+#include "testbeatdetection.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,11 +10,14 @@ int main(int argc, char *argv[])
     //BatchStrainTracker::extractStrains("/mnt/data/Dropbox/projekty/icrc/test2", "nem");
     //BatchStrainTracker::stats("zdr");
     //BatchStrainTracker::stats("nem");
-    BatchStrainTracker::exportValues("zdr");
-    BatchStrainTracker::exportValues("nem");
+    //BatchStrainTracker::exportValues("zdr");
+    //BatchStrainTracker::exportValues("nem");
 
     //BatchStrainTracker::evaluate("nem", "zdr");
     //CreateModel::create("/mnt/data/strain/potkani", "potkani");
+
+    //TestBeatDetection::test();
+    TestBeatDetection::eval();
 
     return 0;
 }
